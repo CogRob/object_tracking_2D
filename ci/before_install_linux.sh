62f59f4736f9b13f3b71ede112ad822d87c6bd2a
ruffsl <roxfoxpox@gmail.com>
2015-03-15 17:37:06 -0400

    Adding Travis Files

---
+++ ci/before_install_linux.sh
@@ -0,0 +1,30 @@
+before_install() {
+  cd /tmp
+  git clone https://github.com/Itseez/opencv.git
+
+  cd /home/dox/opencv/ \
+	&& git checkout 2.4.9 \
+	&& mkdir build \
+	&& cd build \
+	&& cmake -BUILD_opencv_nonfree="True" .. \
+	&& make \
+	&& sudo make install
+}
+
+APT_CORE='
+cmake
+libboost-all-dev
+libglew-dev
+freeglut3-dev
+liblapack3
+build-essential
+libxmu-dev
+libxi-dev
+checkinstall
+libgtk2.0-dev
+pkg-config
+'
+
+sudo apt-get -qq --yes --force-yes install $APT
+
+(before_install)
